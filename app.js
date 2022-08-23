1dc7bab78e3bdf7951c844af54d15b18b26b1f43
Gianmarco Pow-Sang <hello@gpstech.io>
2022-08-23 11:45:23 -0400

    setting up API template

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const express = require("express");
 //adding methods to app variable
 const app = express();
 
+app.use(express.json());
+
 //routing is how an app responds to a client/http request
 // app.get("/", (req, res) => {
 //   res
@@ -27,6 +29,13 @@ app.get("/api/v1/tours", (req, res) => {
       tours,
     },
   });
+});
+
+//post is for data from client to server
+//req object holds all the data about the request
+app.post("/api/v1/tours", (req, res) => {
+  console.log(req.body);
+  res.send("Done");
 });
 
 const port = 3000;
